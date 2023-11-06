1bdf58db819e99a4d1a8193f55badf8f6bab7791
daveakin321 <daveakin321@gmail.com>
2023-11-06 11:32:25 +0000

    Discount repository

--- src/main/java/org/example/repositories/DiscountRepository.java
+++ src/main/java/org/example/repositories/DiscountRepository.java
@@ -1,4 +1,12 @@
-package org.example.repositories;
+package com.example.hotelreservationsystem.repositories;
 
-public interface DiscountRepository {
+import com.example.hotelreservationsystem.models.Discount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DiscountRepository extends JpaRepository<Discount, Long> {
+    Optional<Discount> findByCode(String code);
+    List<Discount> findAllByIsActive(boolean isActive);
 }
