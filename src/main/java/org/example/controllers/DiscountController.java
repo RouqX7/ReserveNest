c0e701aeb8294ce91b2c988aad15f368ab5b5007
David <20262035@studentmail.ul.ie>
2023-11-06 00:53:47 +0000

    Discount and Reservation Controllers

---
+++ src/main/java/org/example/controllers/DiscountController.java
@@ -0,0 +1,53 @@
+package com.example.hotelreservationsystem.controllers;
+
+import com.example.hotelreservationsystem.models.Discount;
+import com.example.hotelreservationsystem.models.DiscountType;
+import com.example.hotelreservationsystem.models.Reservation;
+import com.example.hotelreservationsystem.services.DiscountService;
+
+import java.util.List;
+
+public class DiscountController {
+    private final DiscountService discountService;
+
+    // Constructor injection of the DiscountService
+    public DiscountController(DiscountService discountService) {
+        this.discountService = discountService;
+    }
+
+    // Endpoint to get all active discounts
+    public List<Discount> getActiveDiscounts() {
+        return discountService.getActiveDiscounts();
+    }
+
+    // Endpoint to create a discount
+    public Discount createDiscount(String code, String name, String description, DiscountType discountType, double value, Discount.DiscountCriteria criteria) {
+        return discountService.createDiscount(code, name, description, discountType, value, criteria);
+    }
+
+    // Endpoint to edit a discount
+    public Discount editDiscount(String code, Double newValue, String newDescription) {
+        return discountService.editDiscount(code, newValue, newDescription);
+    }
+
+    // Endpoint to deactivate a discount
+    public void deactivateDiscount(String code) {
+        discountService.deactivateDiscount(code);
+    }
+
+    // Endpoint to apply a discount to a reservation
+    public double applyDiscountToReservation(Long reservationId, String discountCode) {
+        // You'll need to fetch the reservation by ID from your data source here
+        Reservation reservation = fetchReservationById(reservationId);
+        return discountService.applyDiscount(reservation, discountCode);
+    }
+
+    // Method to fetch a reservation by ID (you'll need to implement the logic to fetch from your data source)
+    private Reservation fetchReservationById(Long reservationId) {
+        // Fetching logic (using repository or directly from a data store)
+        // Placeholder for actual implementation
+        throw new UnsupportedOperationException("fetchReservationById not implemented yet.");
+    }
+
+    // Other controller methods as needed...
+}
---
+++ src/main/java/org/example/controllers/ReservationController.java
@@ -0,0 +1,48 @@
+package com.example.hotelreservationsystem.controllers;
+
+import com.example.hotelreservationsystem.models.Discount;
+import com.example.hotelreservationsystem.models.Reservation;
+import com.example.hotelreservationsystem.services.DiscountService;
+import com.example.hotelreservationsystem.services.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+    //    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private final ReservationService reservationService;
+    private final DiscountService discountService;
+
+
+
+//    public void reviewReservationDetails(LocalDate checkInDate, LocalDate checkOutDate, int numberOfGuests, String roomType) {
+//        System.out.println("\nPlease review your reservation details:");
+//        System.out.println("Check-in Date: " + checkInDate.format(dateFormatter));
+//        System.out.println("Check-out Date: " + checkOutDate.format(dateFormatter));
+//        System.out.println("Number of Guests: " + numberOfGuests);
+//        System.out.println("Room Type: " + roomType);
+//        // Include any other details relevant to the reservation
+//
+//        System.out.println("\nIs this information correct? (yes/no): ");
+//        // Here you would implement logic to confirm the reservation or to go back and edit details
+//    }
+
+    @Autowired
+    public ReservationController( ReservationService reservationService, DiscountService discountService) {
+        this.reservationService = reservationService;
+        this.discountService = discountService;
+    }
+    // Endpoint to apply a discount to a reservation
+    @PostMapping("/{reservationId}/discounts/{discountCode}")
+    public Reservation applyDiscount(@PathVariable Long reservationId, @PathVariable String discountCode) {
+        Reservation reservation = reservationService.getReservationById(reservationId);
+        Discount discount = discountService.getDiscountByCode(discountCode); // Assuming you have this method
+        reservationService.applyDiscountToReservation(reservationId, discount);
+        return reservation; // The updated reservation is returned after discount application
+    }
+}
